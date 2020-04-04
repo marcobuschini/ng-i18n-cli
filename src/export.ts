4954cb96a6a45e9816a033b4afe2b339222c9d1d
Marco Buschini <marco.buschini@gmail.com>
2020-04-04 23:35:10 +0200

    feat(export): implement scanning of sources, and partial export of strings from HTML files
    Export from TS files not implemented, yet. Export from HTML only implements scanning of files,
    doesn't generate a file, yet.

--- src/export.ts
+++ src/export.ts
@@ -5,12 +5,12 @@ import * as fs from 'fs'
 export class Export {
   public async export(inputPath: string, outputFile: string) {
     console.log('Loading messages from source files in ' + inputPath)
-    this.fromDir(inputPath, '.ts')
+    this.fromDirTS(inputPath)
     console.log('Loading messages from html files in ' + inputPath)
-    this.fromDir(inputPath, '.html')
+    this.fromDirHTML(inputPath)
   }
 
-  private fromDir(inputPath: string, filter: string) {
+  private fromDirTS(inputPath: string, filter: string = '.ts') {
     if (!fs.existsSync(inputPath)) {
       console.log('no dir ', inputPath)
       return
@@ -21,10 +21,57 @@ export class Export {
       const filename = path.join(inputPath, file)
       const stat = fs.lstatSync(filename)
       if (stat.isDirectory()) {
-        this.fromDir(filename, filter)
+        this.fromDirTS(filename, filter)
       } else if (filename.indexOf(filter) >= 0) {
         console.log('-- found: ', filename)
+        this.parseTS(filename)
       }
     })
+  }
+
+  private fromDirHTML(inputPath: string, filter: string = '.html') {
+    if (!fs.existsSync(inputPath)) {
+      console.log('no dir ', inputPath)
+      return
+    }
+
+    const files = fs.readdirSync(inputPath)
+    files.forEach((file: string) => {
+      const filename = path.join(inputPath, file)
+      const stat = fs.lstatSync(filename)
+      if (stat.isDirectory()) {
+        this.fromDirHTML(filename, filter)
+      } else if (filename.indexOf(filter) >= 0) {
+        console.log('-- found: ', filename)
+        this.parseHTML(filename)
+      }
+    })
+  }
+
+  private parseTS(filename: string): string[] {
+    const ret = new Array<string>()
+
+    return ret
+  }
+
+  private parseHTML(filename: string) {
+    const ret = new Array<string>()
+    const buffer = fs.readFileSync(filename, 'utf8')
+    const regex = /'([A-Z0-9_]+)'\s*\|\s*(translate\s*(.*)|translate)/g
+    let m = regex.exec(buffer)
+    while (m !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++
+      }
+
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        if (groupIndex === 1) {
+          console.log(`Found match: ${match}`)
+        }
+      })
+      m = regex.exec(buffer)
+    }
   }
 }
