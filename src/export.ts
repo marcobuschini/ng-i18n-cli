32a267fad160d3be814e5d782cad981e405c2832
Marco Buschini <marco.buschini@gmail.com>
2020-04-11 15:18:13 +0200

    fix(export): remove duplicated code
    Simplify the structure of the code as per codeclimate reports
    
    fix #7 #8 #9 #10

--- src/export.ts
+++ src/export.ts
@@ -4,9 +4,9 @@ import * as fs from 'fs'
 export class Export {
   public async export(inputPath: string, outputFile: string) {
     console.log(`Loading messages from source files in ${inputPath}`)
-    const ts = this.fromDirTS(inputPath)
+    const ts = this.fromDir(inputPath, '.ts')
     console.log(`Loading messages from html files in ${inputPath}`)
-    const html = this.fromDirHTML(inputPath)
+    const html = this.fromDir(inputPath, '.html')
 
     const uniq = ts
       .concat(html)
@@ -18,12 +18,11 @@ export class Export {
     fs.writeFileSync(outputFile, ret)
   }
 
-  private fromDirTS(inputPath: string, filter: string = '.ts'): string[] {
+  fromDir(inputPath: string, filter: string) {
     if (!fs.existsSync(inputPath)) {
       console.log(`Path not found ${inputPath}`)
       return []
     }
-
     let unsorted = new Array<string>()
 
     const files = fs.readdirSync(inputPath)
@@ -31,64 +30,28 @@ export class Export {
       const filename = path.join(inputPath, file)
       const stat = fs.lstatSync(filename)
       if (stat.isDirectory()) {
-        unsorted = unsorted.concat(this.fromDirTS(filename, filter))
+        unsorted = unsorted.concat(this.fromDir(filename, filter))
       } else if (filename.indexOf(filter) >= 0) {
         console.log(`-- found: ${filename}`)
-        unsorted = unsorted.concat(this.parseTS(filename))
+        unsorted = unsorted.concat(this.parse(filename))
       }
     })
     return unsorted
   }
 
-  private fromDirHTML(inputPath: string, filter: string = '.html'): string[] {
-    if (!fs.existsSync(inputPath)) {
-      console.log(`Path not found ${inputPath}`)
+  private parse(filename: string): string[] {
+    const ret = new Array<string>()
+    const buffer = fs.readFileSync(filename, 'utf8')
+    const regexTS = /translate\s*\(\s*['"]([A-Z0-9_]*)['"]\s*(\s*.*)\)/g
+    const regexHTML = /'([A-Z0-9_]+)'\s*\|\s*(translate\s*(.*)|translate)/g
+    let regex: RegExp
+    if (filename.endsWith('.ts')) {
+      regex = regexTS
+    } else if (filename.endsWith('.html')) {
+      regex = regexHTML
+    } else {
       return []
     }
-
-    let unsorted = new Array<string>()
-
-    const files = fs.readdirSync(inputPath)
-    files.forEach((file: string) => {
-      const filename = path.join(inputPath, file)
-      const stat = fs.lstatSync(filename)
-      if (stat.isDirectory()) {
-        unsorted = unsorted.concat(this.fromDirHTML(filename, filter))
-      } else if (filename.indexOf(filter) >= 0) {
-        console.log(`-- found: ${filename}`)
-        unsorted = unsorted.concat(this.parseHTML(filename))
-      }
-    })
-    return unsorted
-  }
-
-  private parseTS(filename: string): string[] {
-    const ret = new Array<string>()
-    const buffer = fs.readFileSync(filename, 'utf8')
-    const regex = /translate\s*\(\s*['"]([A-Z0-9_]*)['"]\s*(\s*.*)\)/g
-    let m = regex.exec(buffer)
-    while (m !== null) {
-      // This is necessary to avoid infinite loops with zero-width matches
-      if (m.index === regex.lastIndex) {
-        regex.lastIndex++
-      }
-
-      // The result can be accessed through the `m`-variable.
-      m.forEach((match, groupIndex) => {
-        if (groupIndex === 1) {
-          console.log(`Found match: ${match}`)
-          ret.push(match)
-        }
-      })
-      m = regex.exec(buffer)
-    }
-    return ret
-  }
-
-  private parseHTML(filename: string): string[] {
-    const ret = new Array<string>()
-    const buffer = fs.readFileSync(filename, 'utf8')
-    const regex = /'([A-Z0-9_]+)'\s*\|\s*(translate\s*(.*)|translate)/g
     let m = regex.exec(buffer)
     while (m !== null) {
       // This is necessary to avoid infinite loops with zero-width matches
